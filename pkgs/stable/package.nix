390d139d68f611cf527cc86aa89b98ca2f6e7cba
Rishabh5321 <rishabh98818@outlook.com>
2025-07-07 07:28:21 +0530

    Inherit from base package in stable and nightly variants

--- pkgs/stable/package.nix
+++ pkgs/stable/package.nix
@@ -1,66 +1,25 @@
 { lib
-, buildNpmPackage
-, fetchFromGitHub
-, fetchNpmDeps
-, writeShellApplication
-, cacert
-, curl
-, gnused
-, jq
-, nix-prefetch-github
-, prefetch-npm-deps
-, gitUpdater
+, pkgs
 ,
 }:
 
-buildNpmPackage (finalAttrs: {
-  pname = "gemini-cli";
-  version = "0.1.7";
+pkgs.gemini-cli.overrideAttrs (
+  finalAttrs: prevAttrs: {
+    version = "0.1.7";
 
-  src = fetchFromGitHub {
-    owner = "google-gemini";
-    repo = "gemini-cli";
-    tag = "v${finalAttrs.version}";
-    hash = "sha256-DAenod/w9BydYdYsOnuLj7kCQRcTnZ81tf4MhLUug6c=";
-  };
+    src = pkgs.fetchFromGitHub {
+      owner = "google-gemini";
+      repo = "gemini-cli";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-DAenod/w9BydYdYsOnuLj7kCQRcTnZ81tf4MhLUug6c=";
+      postFetch = ''
+        ${lib.getExe pkgs.npm-lockfile-fix} $out/package-lock.json
+      '';
+    };
 
-  npmDeps = fetchNpmDeps {
-    inherit (finalAttrs) src;
-    hash = "sha256-otogkSsKJ5j1BY00y4SRhL9pm7CK9nmzVisvGCDIMlU=";
-  };
-
-  preConfigure = ''
-    mkdir -p packages/generated
-    echo "export const GIT_COMMIT_INFO = { commitHash: '${finalAttrs.src.rev}' };" > packages/generated/git-commit.ts
-  '';
-
-  installPhase = ''
-    runHook preInstall
-    mkdir -p $out/{bin,share/gemini-cli}
-
-    cp -r node_modules $out/share/gemini-cli/
-
-    rm -f $out/share/gemini-cli/node_modules/@google/gemini-cli
-    rm -f $out/share/gemini-cli/node_modules/@google/gemini-cli-core
-    cp -r packages/cli $out/share/gemini-cli/node_modules/@google/gemini-cli
-    cp -r packages/core $out/share/gemini-cli/node_modules/@google/gemini-cli-core
-
-    ln -s $out/share/gemini-cli/node_modules/@google/gemini-cli/dist/index.js $out/bin/gemini
-    runHook postInstall
-  '';
-
-  postInstall = ''
-    chmod +x "$out/bin/gemini"
-  '';
-
-  passthru.updateScript = gitUpdater { };
-
-  meta = {
-    description = "AI agent that brings the power of Gemini directly into your terminal";
-    homepage = "https://github.com/google-gemini/gemini-cli";
-    license = lib.licenses.asl20;
-    maintainers = with lib.maintainers; [ ];
-    platforms = lib.platforms.all;
-    mainProgram = "gemini";
-  };
-})
+    npmDeps = pkgs.fetchNpmDeps {
+      inherit (finalAttrs) src;
+      hash = "sha256-otogkSsKJ5j1BY00y4SRhL9pm7CK9nmzVisvGCDIMlU=";
+    };
+  }
+)
